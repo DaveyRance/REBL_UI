0a0333edeec3f813ccdd10af298f25a15f2b305a
Adam <adamaero@hotmail.com>
2018-08-15 08:49:39 -0500

    Update REBLDisplay.h

--- REBLDisplay.h
+++ REBLDisplay.h
@@ -7,12 +7,12 @@
 #include "Defines.h"
 
 enum color_vars {BLACK, RED , GREEN , YELLOW , BLUE , PURPLE , CYAN , WHITE};
-#define LCD_ENABLE_PIN A0 // New ↓
-#define LCD_RS_PIN A1
-#define LCD_D1 5    
-#define LCD_D2 4
-#define LCD_D3 3
-#define LCD_D4 7
+// #define LCD_ENABLE_PIN A0 // New ↓
+// #define LCD_RS_PIN A1
+// #define LCD_D1 5    
+// #define LCD_D2 4
+// #define LCD_D3 3
+// #define LCD_D4 7
 
 void initLCD(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_D1, LCD_D2, LCD_D3, LCD_D4); // New
 void doBacklight();
